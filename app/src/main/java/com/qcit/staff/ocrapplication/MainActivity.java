1e41bc3f4fe30521a1cf9edcb1e894dba093f04e
lib093 <li.2121>
2021-03-09 13:24:34 +0800

    tijiao

---
+++ app/src/main/java/com/qcit/staff/ocrapplication/MainActivity.java
@@ -0,0 +1,181 @@
+package com.qcit.staff.ocrapplication;
+
+import com.baidu.ocr.sdk.OCR;
+import com.baidu.ocr.sdk.OnResultListener;
+import com.baidu.ocr.sdk.exception.OCRError;
+import com.baidu.ocr.sdk.model.AccessToken;
+import com.baidu.ocr.sdk.model.IDCardParams;
+import com.baidu.ocr.sdk.model.IDCardResult;
+import com.baidu.ocr.ui.camera.CameraActivity;
+import com.baidu.ocr.ui.camera.CameraNativeHelper;
+import com.baidu.ocr.ui.camera.CameraView;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private boolean hasGotToken = false;
+
+    private AlertDialog.Builder alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alertDialog = new AlertDialog.Builder(this);
+        initAccessTokenLicenseFile();
+
+
+
+
+
+
+
+        // 身份证识别
+        findViewById(R.id.idcard_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!checkTokenStatus()) {
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH,
+                        new File(MainActivity.this.getFilesDir(), "pic.jpg").getAbsolutePath());
+                intent.putExtra(CameraActivity.KEY_NATIVE_ENABLE,
+                        true);
+                // KEY_NATIVE_MANUAL设置了之后CameraActivity中不再自动初始化和释放模型
+                // 请手动使用CameraNativeHelper初始化和释放模型
+                // 推荐这样做，可以避免一些activity切换导致的不必要的异常
+                intent.putExtra(CameraActivity.KEY_NATIVE_MANUAL,
+                        true);
+                intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_ID_CARD_FRONT);
+                startActivityForResult(intent, 100);
+            }
+        });
+
+
+        // 请选择您的初始化方式
+        //initAccessToken();
+        //initAccessTokenWithAkSk();
+
+    }
+
+    private boolean checkTokenStatus() {
+        if (!hasGotToken) {
+            Toast.makeText(getApplicationContext(), "token还未成功获取", Toast.LENGTH_LONG).show();
+        }
+        return hasGotToken;
+    }
+
+
+    /**
+     * 自定义license的文件路径和文件名称，以license文件方式初始化
+     */
+    private void initAccessTokenLicenseFile() {
+        OCR.getInstance(this).initAccessToken(new OnResultListener<AccessToken>() {
+            @Override
+            public void onResult(AccessToken accessToken) {
+                String token = accessToken.getAccessToken();
+                hasGotToken = true;
+
+                //  初始化本地质量控制模型,释放代码在onDestory中
+                //  调用身份证扫描必须加上 intent.putExtra(CameraActivity.KEY_NATIVE_MANUAL, true); 关闭自动初始化和释放本地模型
+                CameraNativeHelper.init(MainActivity.this, OCR.getInstance(MainActivity.this).getLicense(),
+                        new CameraNativeHelper.CameraNativeInitCallback() {
+                            @Override
+                            public void onError(int errorCode, Throwable e) {
+                                String msg;
+                                switch (errorCode) {
+                                    case CameraView.NATIVE_SOLOAD_FAIL:
+                                        msg = "加载so失败，请确保apk中存在ui部分的so";
+                                        break;
+                                    case CameraView.NATIVE_AUTH_FAIL:
+                                        msg = "授权本地质量控制token获取失败";
+                                        break;
+                                    case CameraView.NATIVE_INIT_FAIL:
+                                        msg = "本地质量控制";
+                                        break;
+                                    default:
+                                        msg = String.valueOf(errorCode);
+                                }
+                                Log.d("lib",msg);
+                            }
+                        });
+            }
+
+            @Override
+            public void onError(OCRError error) {
+                error.printStackTrace();
+                alertText("自定义文件路径licence方式获取token失败", error.getMessage());
+            }
+        }, "aip.license", getApplicationContext());
+    }
+
+
+    private void alertText(final String title, final String message) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                alertDialog.setTitle(title)
+                        .setMessage(message)
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+        });
+    }
+
+    private void infoPopText(final String result) {
+        alertText("", result);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initAccessTokenLicenseFile();
+        } else {
+            Toast.makeText(getApplicationContext(), "需要android.permission.READ_PHONE_STATE", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                String contentType = data.getStringExtra(CameraActivity.KEY_CONTENT_TYPE);
+                if (!TextUtils.isEmpty(contentType)) {
+                    if (CameraActivity.CONTENT_TYPE_ID_CARD_FRONT.equals(contentType)) {
+                        Toast.makeText(MainActivity.this,data.getStringExtra("result"),Toast.LENGTH_LONG).show();
+                    } else if (CameraActivity.CONTENT_TYPE_ID_CARD_BACK.equals(contentType)) {
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        // 释放本地质量控制模型
+        CameraNativeHelper.release();
+        super.onDestroy();
+        // 释放内存资源
+        OCR.getInstance(this).release();
+    }
+}
